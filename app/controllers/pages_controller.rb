4a52f44ac5729c09cc9bac578f63b1014e0b4eb8
thinker1028 <webdev1028@gmail.com>
2020-12-21 22:39:37 -0800

    commit result

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,26 @@
+class PagesController < ApplicationController
+    def demo_food_blog
+    end
+    def blog_single_ls
+    end
+    def blog_single_ns
+    end
+    def blog_single_rs
+    end
+    def portfolio_single_side_gallery_list
+    end
+    def dashboard_followers
+    end
+    def dashboard_favourites
+    end
+    def signin_image
+    end
+    def account_profile
+    end
+    def signin_signup
+    end
+    def help_submit_request
+    end
+    def index
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+  get 'pages/demo-food-blog'
+  get 'pages/blog-single-ls'
+  get 'pages/blog-single-ns'
+  get 'pages/blog-single-rs'
+  get 'pages/portfolio-single-side-gallery-list'
+  get 'pages/dashboard-followers'
+  get 'pages/dashboard-favorites'
+  get 'pages/signin-image'
+  get 'pages/account-profile'
+  get 'pages/signin-signup'
+  get 'pages/help-submit-request'
+  root "pages#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
